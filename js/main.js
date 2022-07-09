bd4c73ba0979cf53dae18e63d1c03ee65daebf92
tarik2454 <tarik2454@gmail.com>
2022-07-09 17:05:29 +0300

    layout of the work, blog and contact blocks

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+// -- slider 
+new Swiper('.swiper1', {
+    slidesPerView: 'auto',
+    loop: true,
+    loopedSlides: 2,
+    spaceBetween: 300,
+    centeredSlides: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+new Swiper('.swiper2', {
+    slidesPerView: 'auto',
+    loop: true,
+    loopedSlides: 2,
+    spaceBetween: 300,
+    centeredSlides: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
