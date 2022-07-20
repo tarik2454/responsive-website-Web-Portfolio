c45efe34f32863cf3151a612b317fdeb4634a3ca
tarik2454 <tarik2454@gmail.com>
2022-07-20 16:54:52 +0300

    writing JavaScript and other style changes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,91 @@
 
-// -- slider 
-new Swiper('.swiper1', {
-    slidesPerView: 'auto',
-    loop: true,
-    loopedSlides: 2,
-    spaceBetween: 500,
-    centeredSlides: true,
+// -- navToggle
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('#nav');
+const navLink = document.querySelectorAll('.nav__link');
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+navToggle.addEventListener('click', function() {
+    this.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+
+navLink.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        setTimeout(function() {
+            navToggle.classList.remove('active');
+            nav.classList.remove('active');
+        }, 300);    
+    });  
 });
 
 
+// -- header fixed
+const header = document.querySelector('#header');
+const intro = document.querySelector('#intro');
+const introH = intro.offsetHeight;
+
+window.onscroll = function() {
+    if (window.pageYOffset >= introH - 50) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+};
+
+
+// -- scroll to
+const scrollBtn = document.querySelectorAll('[data-scroll]');
+
+scrollBtn.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const scrollId = document.querySelector('#' + this.dataset.scroll);
+        const scrollTop = scrollId.offsetTop - 50;
+
+        window.scrollTo({ top: scrollTop, behavior: 'smooth'});
+    });
+});
+
+
+// -- filter 
+const filterBtn = document.querySelectorAll('[data-filter]');
+const dataCat = document.querySelectorAll('[data-cat]');
+const btn = document.querySelector('.btn').offsetHeight;
+console.log(btn)
+
+for (let i = 8; i < dataCat.length; i++) { 
+    dataCat[i].classList.add('hide'); 
+};
+
+filterBtn.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+      
+        const cat = this.dataset.filter;
+
+        if(cat == 'ALL') {
+            dataCat.forEach(function(item) {
+                item.classList.remove('hide');
+            });
+        } else {
+            dataCat.forEach(function(item) {
+                const workCat = item.dataset.cat;
+ 
+                if(workCat != cat) {
+                    item.classList.add('hide');
+                } else {
+                    item.classList.remove('hide');
+                }  
+            });  
+        };    
+    });
+});
+
+
+// -- slider 
 new Swiper('.swiper2', {
     slidesPerView: 'auto',
     loop: true,
@@ -27,5 +99,3 @@ new Swiper('.swiper2', {
     },
 });
 
-const hed = document.querySelector('.header').offsetHeight;
-console.log(hed)
